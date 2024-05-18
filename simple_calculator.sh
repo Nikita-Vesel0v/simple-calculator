555851713f8bad678169d82e31dad71f61e543ab
Nikita-Vesel0v <nikiveselov2004@gmail.com>
2024-05-18 09:42:36 +0300

    add arithmetic operation

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash 
+
 echo "Enter an arithmetic operation: "
-read eq
-re='^[-0-9 ]+ [-,+,*,/] [-0-9]+$'
-if [[ $eq =~ $re ]]; then
-    echo "Operation check passed!"
+read a operator b
+
+# Regex for checking integer number and operators
+re_num='^-?[0-9]+$'
+re_operator='^[-+*/]$'
+
+# Checking for the data
+if [[ $a =~ $re_num ]] && [[ $b =~ $re_num ]] && [[ $operator =~ $re_operator ]]; then
+    # Do arithmetic operation
+    arithmetic_result=$(( a $operator b ))
+    printf "%s\n" "$arithmetic_result"
 else
     echo "Operation check failed!"
 fi
